f91c4604f72d33da49d338f3d3b5f4831f1de88a
Colin Barber <colinbmusic@gmail.com>
2018-02-18 11:49:13 -0500

    updated the traffic with toggle functionality

--- Arduino/Smart-Traffic/Smart-Traffic.ino
+++ Arduino/Smart-Traffic/Smart-Traffic.ino
@@ -9,14 +9,26 @@ void setup() {
 
 }
 
+bool trafficState = LOW;
+bool held = LOW;
 int check = 0;
+bool flip = LOW;
 
 void loop() {
 
 // touch sensor
 
   int sensorValue = analogRead(A0);
+  
   if (sensorValue >= 1000) {
+    if (held == LOW){
+    trafficState = !trafficState;
+    held = HIGH;}   
+  }
+  else {
+    held = LOW;
+  }
+if (trafficState == HIGH) {  
     digitalWrite(7, LOW);
     digitalWrite(8, HIGH);                      
     digitalWrite(9, HIGH);    
@@ -24,37 +36,33 @@ void loop() {
   else {
     digitalWrite (7, HIGH);
     digitalWrite(8, LOW);                      
-    if (check < 2) digitalWrite(9, LOW);
-  }
-
-
-// light sensor
+    if (check <2) digitalWrite(9, LOW);
 
   int sensorValue2 = analogRead(A1);
   if (sensorValue2 >= 1000) {
     switch (check) {
-    case 0:
-    digitalWrite(9, LOW); 
+    case 0: 
     check++;
-    delay (550);
+    delay (560);
     break;
-    case 1:
-    digitalWrite(9, LOW); 
+    case 1: 
     check--;
-    delay (550);
+    delay (560);
     break;
-    case 2: 
+    case 2:
+    flip = HIGH; 
+    digitalWrite(9, HIGH);
     check++;
-    delay (550);
+    delay (560);
     break;
     case 3:
-    digitalWrite(9, HIGH); 
-    check++;
-    delay (550);
-    break;
-    case 4: 
+    if (flip == HIGH){
+       check--;
+    }
+    else {
     digitalWrite(9, LOW);
     check = 0;
+    delay (560);}
     break;
   }}
   else {
@@ -62,29 +70,39 @@ void loop() {
     case 0: 
     digitalWrite (9, LOW);
     break;
-    case 1:
-    digitalWrite(9, LOW); 
+    case 1: 
     check++;
-    delay (550);
+    delay (560);
     break;
-    case 2: 
+    case 2:
+    flip = LOW; 
     digitalWrite(9, HIGH);
     check++;
-    delay (550);
+    delay (560);
     break;
     case 3:
-    digitalWrite(9, HIGH); 
-    check--;
-    delay (550);
-    break;
-    case 4: 
-    check--;
-    delay (550);
+    if (flip == HIGH){ 
+      check--;
+      delay (560);}
+    else {
+      check = 0;
+    }
     break;
   }
   }
+    Serial.print("check = ");
     Serial.print(check);
     Serial.println();
 
+    Serial.print("flip = ");
+    Serial.print(flip);
+    Serial.println();
+
+  
+  }
+
+// light sensor
+
+ 
 
 }
